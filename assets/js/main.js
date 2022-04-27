8d45d3e303e79fd830a980f298230701c59637f7
devwulff <p.wulff887@gmail.com>
2022-04-27 10:52:04 +0200

    Javascript updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,7 @@ let inputEmail = document.getElementById('inputemail');
 let inputMessage = document.getElementById('message')
 
 function submitForm() {
-    let name = inputName.value;
-    let email = inputEmail.value;
-    let message = inputMessage.value;
-    console.log(name);
-    console.log(email);
-    console.log(message);
+    console.log(inputName.value);
+    console.log(inputEmail.value);
+    console.log(inputMessage.value);
 }
